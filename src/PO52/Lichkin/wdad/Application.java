3daa5e91d54713b0e5fbee569deee24ba10a2be9
Wearux <ukoz_sam@mail.ru>
2017-09-20 19:50:15 +0400

    Added entry point

---
+++ src/PO52/Lichkin/wdad/Application.java
@@ -0,0 +1,10 @@
+package PO52.Lichkin.wdad;
+
+/**
+ * Created by D on 20.09.2017.
+ */
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("I’m Andrey Lichkin, and I’m not a monkey");
+    }
+}
