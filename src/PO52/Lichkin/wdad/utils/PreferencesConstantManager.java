84118fbd753dd9db4d6fccfad535269d82dad9a5
Wearux <ukoz_sam@mail.ru>
2017-11-01 20:57:32 +0400

    PreferencesManagerAPIChange

---
+++ src/PO52/Lichkin/wdad/utils/PreferencesConstantManager.java
@@ -0,0 +1,10 @@
+package PO52.Lichkin.wdad.utils;
+
+public interface PreferencesConstantManager {
+    String CREATEREGISTRY="appconfig.rmi.server.registry.createregistry";
+    String REGISTRYADDRESS="appconfig.rmi.server.registry.registryaddress";
+    String REGISTRYPORT="appcofig.rmi.server.registry.registryport";
+    String POLICYPATH="appconfig.rmi.client.policypath";
+    String USECODEBASEONLY="appconfig.rmi.client.usecodebaseonly";
+    String CLASSPROVIDER="appconfig.rmi.classprovider";
+}
